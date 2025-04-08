11830ba1b850d56ec4eae0398fc41af022058142
haolin <c21325063@mytudublin.ie>
2025-04-08 14:55:42 +0100

    remove unused service

--- backend/src/main/java/com/recipefind/backend/service/FatSecretService.java
+++
@@ -1,7 +0,0 @@
-package com.recipefind.backend.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface FatSecretService {
-    public String searchRecipes (String keyword) throws JsonProcessingException;
-}
